4cd4fe63a250d74ea5c02731d0d5299d06865f7b
Harry Hsu <qspiaryb@gmail.com>
2024-02-22 12:55:19 +0800

    Add https

--- src/api.js
+++ src/api.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const app = express();
+const cert = fs.readFileSync("cert.pfx");
 
 app.use(express.static("public"));
 
 const port = process.env.port;
-module.exports = app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+module.exports = https
+  .createServer(
+    {
+      pfx: cert,
+      passphrase: "1234",
+    },
+    app
+  )
+  .listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
